41836ad5e992a18c1dc08fcc0efc265547469d0b
Saloni Pandey <salonipandey014@gmail.com>
2024-03-23 21:45:02 +0530

    Created Portfolio Assignment

--- src/Sections/Testi.jsx
+++ src/Sections/Testi.jsx
@@ -1,49 +1,47 @@
-import { Swiper, SwiperSlide } from "swiper/react";
 import { useLoaderData } from "react-router-dom";
-import { FreeMode, Pagination } from "swiper/modules";
-import { motion } from "framer-motion"
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/free-mode";
+import Slider from "react-slick";
+import { motion } from "framer-motion";
 import { RxArrowTopRight } from "react-icons/rx";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Testi = () => {
   const { user } = useLoaderData();
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3, // Adjust based on your breakpoints
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <motion.div className="flex items-center justify-center flex-col h-screen overflow-hidden bg-black"
-    initial={{ y: "-500vh" }}
+    <motion.div
+      className="flex items-center justify-center flex-col h-screen overflow-hidden bg-black"
+      initial={{ y: "-500vh" }}
       animate={{ y: "0%" }}
-      transition={{ duration: 1.5 }}>
-      <Swiper
-        style={{
-          "--swiper-pagination-color": "#cc688c",
-          "--swiper-pagination-bullet-inactive-color": "#fff",
-          
-        }}
-        breakpoints={{
-          320: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-          },
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 15,
-          },
-          1024: {
-            slidesPerView: 3,
-            spaceBetween: 20,
-          },
-        }}
-        freeMode={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[FreeMode, Pagination]}
-        className="w-full max-w-[90%] lg:max-w-[80%]"
-      >
+      transition={{ duration: 1.5 }}
+    >
+      <Slider {...settings} className="w-full max-w-[90%] lg:max-w-[80%]">
         {user.testimonials.map((testimonial) => (
-          <SwiperSlide key={testimonial._id}>
+          <div key={testimonial._id}>
             <div className="flex flex-col gap-6 mb-20 group relative shadow-lg text-white rounded-xl px-6 py-8 lg:px-8 lg:py-12 lg:h-[400px] overflow-hidden cursor-pointer">
               <div
                 className="absolute inset-0 bg-cover bg-center border"
@@ -64,9 +62,9 @@ const Testi = () => {
               </div>
               <RxArrowTopRight className="absolute bottom-5 left-5 w-[35px] h-[35px] text-white group-hover:text-[#cc866c] group-hover:rotate-45 duration-100" />
             </div>
-          </SwiperSlide>
+          </div>
         ))}
-      </Swiper>
+      </Slider>
     </motion.div>
   );
 };
